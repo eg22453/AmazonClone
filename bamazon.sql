2404411d916607d994c252b35395ff083667f306
Steven Garza (eg22453) <stevenmacgarza@gmail.com>
2019-05-17 15:01:55 -0500

    make initial sql products table

--- bamazon.sql
+++ bamazon.sql
@@ -4,12 +4,12 @@ CREATE DATABASE bamazon_db;
 
 USE bamazon_db;
 
-CREATE TABLE auctionitems (
-  id INT NOT NULL AUTO_INCREMENT,
-  itemname VARCHAR(45) NULL,
-  category DECIMAL(10,2) NULL,
-  bidprice INT NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(50) NOT NULL,
+  department_name VARCHAR(50) NOT NULL,
+  stock_quantity INT NOT NULL,
+  PRIMARY KEY (item_id)
 );
 
 INSERT INTO auctionitems (itemname, category, bidprice)
