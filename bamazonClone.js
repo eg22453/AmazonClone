e1d737892a732a536aaa14ead0c2661377f11ccc
Steven Garza (eg22453) <stevenmacgarza@gmail.com>
2019-05-18 00:03:02 -0500

    update sql table with new inventory after purchase

--- bamazonClone.js
+++ bamazonClone.js
@@ -1,8 +1,7 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-var Table = require('cli-table');
-var colors = require('colors');
-
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const Table = require('cli-table');
+const colors = require('colors');
 const divider = "------------------------------------------------------------------------------------------"
 
 var connection = mysql.createConnection({
@@ -23,29 +22,29 @@ console.log("\n Hello! Welcome to Bamazon!! Below you will see the products that
 console.log(divider)
 
 connection.connect(function (err) {
+  if (err) throw err;
+
+  var table = new Table({
+    head: ['Item ID', 'Product Name', 'Department Name', 'Price', 'Quantity'],
+    style: {
+      head: ['green']
+    },
+  });
+  //Select all customers and return the result object:
+  connection.query("SELECT * FROM products", function (err, rows, fields) {
     if (err) throw err;
+    for (var i = 0; i < rows.length; i++) {
+      //push each row to our formatted table
+      table.push([rows[i].item_id, rows[i].product_name, rows[i].department_name, rows[i].price, rows[i].stock_quantity]);
+    }
+    //display table to console
+    console.log(table.toString() + "\n")
+    promptuser()
 
-    var table = new Table({
-      head: ['Item ID', 'Product Name', 'Department Name', 'Price', 'Quantity'],
-      style: {
-        head: ['green']
-      },
-    });
-    //Select all customers and return the result object:
-    connection.query("SELECT * FROM products", function (err, rows, fields) {
-      if (err) throw err;
-      for (var i = 0; i < rows.length; i++) {
-        //push each row to our formatted table
-        table.push([rows[i].item_id, rows[i].product_name, rows[i].department_name, rows[i].price, rows[i].stock_quantity]);
-      }
-      //display table to console
-      console.log(table.toString() + "\n")
-      promptuser()
-    });
-    connection.end();
+  });
 
-  }
-);
+});
+
 
 
 //function to ask user which item and what quantity they want to buy
@@ -53,7 +52,7 @@ function promptuser() {
   inquirer
     .prompt([{
         name: "itemid",
-        message: "Which Item ID would you like to buy?"
+        message: "Please type the Item ID of the product you would like to purchase."
       },
       {
         name: "quantitytobuy",
@@ -61,7 +60,27 @@ function promptuser() {
       }
     ])
     .then(res => {
-      console.log(res.response)
-      // Use user feedback for... whatever!!
-    });
+      connection.query("SELECT * FROM products WHERE item_id =?", [res.itemid], function (err, rows, fields) {
+        if (err) throw err;
+
+        if (rows[0].stock_quantity < parseInt(res.quantitytobuy)) {
+          console.log("Insufficient Quantity!!! Try again")
+          promptuser()
+          console.log("passed check")
+        } else {
+          //if purchase is legal then we make a variable to hold new updated inventory 
+          let newstockq = rows[0].stock_quantity - parseInt(res.quantitytobuy)
+          //now we update our SQL table to reflect the purchase
+          connection.query("UPDATE products SET stock_quantity =? WHERE item_id =?", [newstockq, res.itemid], function (err, rows, fields) {
+            if (err) throw err;
+            
+            console.log(newstockq)
+
+          })
+
+          //update table etc...
+        }
+        // Use user feedback for... whatever!!
+      });
+    })
 }
